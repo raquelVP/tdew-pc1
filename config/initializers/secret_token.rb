6f8d3d45b5dbd47f55674151958237875e249965
Raquel Velasquez <raqui140@gmail.com>
2012-03-15 20:55:17 -0500

    solucion pregunta 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdewPc1::Application.config.secret_token = 'ac4cd653d5073a2f24eff973d692ebd0ef8a4183746f906fca79457952535becdb48bf8df6bd4d4a067b534400477eea2ffa54f6147da1d98a8a048f33f059a2'
